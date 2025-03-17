3578f9617f93a7f4e2d7db08d65d58624a9f6ddd
Tomasz Piotrowski <adinsisco@gmail.com>
2025-03-17 15:17:28 +0100

    req

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-# from moviepy.editor import VideoFileClip
-#import requests
-# import openai
-# from dotenv import load_dotenv
-#import os
+from moviepy.editor import VideoFileClip
+import requests
+import openai
+from dotenv import load_dotenv
+import os
 
-import moviepy
-st.write(f"Zainstalowana wersja streamlit: {st.__version__}")
-st.write(f"Zainstalowana wersja moviepy: {moviepy.__version__}")
+# import moviepy
+# st.write(f"Zainstalowana wersja streamlit: {st.__version__}")
+# st.write(f"Zainstalowana wersja moviepy: {moviepy.__version__}")
 
